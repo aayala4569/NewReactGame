7795caa96afdee3a39e182f7a0578b56d871c38f
aayala4569 <angelicaayala100@gmail.com>
2023-08-02 18:29:34 -0700

    Added Responsive Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import {Button, ButtonGroup } from '@chakra-ui/react'
-import './App.css'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-return <Button colorScheme='blue'>Button</Button>
+  // return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `'nav' 'main'`,
+        lg: `'nav nav' 'aside main'`, //This will be at 1024px
+      }}
+    >
+      <GridItem area="nav" bg="orange">
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area="aside" bg="gold">
+        Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
